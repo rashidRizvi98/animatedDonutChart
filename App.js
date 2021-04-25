3f931ab083d06055809d40457c63f59805fd6769
rashidRizvi98 <rashid123321a@gmail.com>
2021-04-26 01:33:08 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DonutChart from "./DonutChart";
+
+const data = [
+  {
+    percentage: 8,
+    color: "tomato",
+    max: 10,
+  },
+  {
+    percentage: 14,
+    color: "skyblue",
+    max: 20,
+  },
+  {
+    percentage: 92,
+    color: "gold",
+    max: 100,
+  },
+  {
+    percentage: 240,
+    color: "#222",
+    max: 500,
+    radius: 200,
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        {data.map((p, i) => {
+          return (
+            <DonutChart
+              key={i}
+              percentage={p.percentage}
+              color={p.color}
+              radius={p.radius}
+              delay={1000}
+              max={p.max}
+              strokeWidth={p.strokeWidth}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 }
@@ -14,8 +60,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
